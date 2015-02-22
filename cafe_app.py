3b6fa20f4e79bba93c212056875e64154ebe11b9
Lucas Desgouilles <ldesgoui@5555>
2015-02-22 04:34:15 +0100

    Jade templating, simple stuff working

--- cafe_app.py
+++ cafe_app.py
@@ -2,28 +2,46 @@ from flask import Flask, render_template, redirect, url_for
 import arrow
 
 app = Flask(__name__)
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+
+def get_coffee_time():
+    try:
+        with open('next_coffee') as fd:
+            next_coffee_stamp = fd.read()
+        next_coffee = arrow.get(next_coffee_stamp)
+    except:
+        set_coffee_time(4242)
+        return get_coffee_time()
+    return next_coffee
+
+
+def set_coffee_time(hour, minute=None):
+    if (minute is None):
+        next_coffee = arrow.get(hour)
+    else:
+        next_coffee = arrow.now().replace(hour=hour,
+                                          minute=minute,
+                                          second=0)
+    with open("next_coffee", 'w+') as fd:
+        fd.write(str(next_coffee.timestamp))
+
 
 @app.route("/")
-def index():
-        with open('next_coffee') as fd:
-                next_coffee_stamp = fd.read()
-        del fd
-        next_coffee = arrow.get(next_coffee_stamp)
-        now = utcnow()
-        when = next_coffee.humanize(now)
-        return render_template('index.html', *locals())
+def index_view():
+    coffee_time = get_coffee_time().to('local')
+    now = arrow.now()
+    humanized = coffee_time.humanize(now, locale='fr_FR')
+    coffee_time = coffee_time.format("HH:mm:ss")
+    now = now.format("HH:mm:ss")
+    return render_template('index.jade', **locals())
 
-@app.route("/set/<when>")
-def set_next_coffee(when):
-        next_coffee = arrow.get(when)
-        with open("next_coffe") as fd:
-                fd.write(next_coffee.timestamp)
-        del fd
-        return redirect(url_for('index'))
-
-
+@app.route("/set/<int:hour>:<int:minute>")
+def set_view(hour, minute):
+    set_coffee_time(hour, minute)
+    return redirect(url_for('index_view'))
 
 application = app
 
 if __name__ == "__main__":
-        app.run()
+        app.run(debug=True)
